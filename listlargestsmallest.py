4284833509185298c7cdfb9a22d3d1cd14af024a
subash2378 <raisubash2378@gmail.com>
2024-08-23 17:51:03 +0545

    go

---
+++ listlargestsmallest.py
@@ -0,0 +1,8 @@
+a=[30,45,40,55,50]
+for i in range(5):
+#     if a[0]<a[i]:
+#         a[0]=a[i]
+# print(f'{a[0]} is the largest number')
+      if a[0]>a[i]:
+          a[0]=a[i]
+print(f'{a[0]} is the smallest number')
